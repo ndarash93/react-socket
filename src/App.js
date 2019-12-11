78b0610173d3a7acf4f75676cd52f1d51e9a83df
ndarash93 <nickdarash@gmail.com>
2019-12-10 21:42:27 -0700

    No comment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-const socket = require("socket.io-client")();
+const socket = require("socket.io-client")('http://192.168.0.31:27817');
 
 function App() {
   const [temp, setTemp] = useState(0);
@@ -8,10 +8,19 @@ function App() {
     setTemp(temp + 1);
   }
 
+  function handleSocket(event) {
+    event.preventDefault();
+    console.log('test');
+    socket.emit('message', 'test');
+  }
+
   return (
     <div className="App">
-      {temp}
-      <button onClick={handleClick}>Click Me!</button>
+      <div className="messages">Test</div>
+      <form>
+        <input type="text" className="text" name="text" />
+        <button type="submit" onClick={handleSocket} >Test</button>
+      </form>
     </div>
   );
 }
